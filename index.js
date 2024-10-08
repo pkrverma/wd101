afc0f528abff04fd8d445fa356f513d9e489f350
Pulkit Kumar Verma <116434106+pkrverma@users.noreply.github.com>
2024-10-09 00:53:52 +0530

    Add files via upload

--- index.js
+++ index.js
@@ -40,8 +40,8 @@ const displayEntries = () => {
     <th class="px-4 py-2">Name</th>
     <th class="px-4 py-2">Email</th>
     <th class="px-4 py-2">Password</th>
-    <th class="px-4 py-2">dob</th>
-    <th class="px-4 py-2">accepted terms?</th>
+    <th class="px-4 py-2">Dob</th>
+    <th class="px-4 py-2">Accepted terms?</th>
     </tr>${tableEntries} </table>`;
 
     let details = document.getElementById("user-entries");
